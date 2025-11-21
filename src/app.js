d5c7078d3f013b8f6e08537844eaa63a56a06143
Sarthak Shukla <sarthakshukla9999@gmail.com>
2025-11-21 22:24:29 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
+import e from "express";
 import express from "express"
-
+import cors from "cors";   
 
 const app = express();
+
+// bassic configurations
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({ extended: true, limit: '16kb'}));
+app.use(express.static('public'));
+
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],  
+}));
+
+
+
 app.get('/', (req, res) =>    { 
     res.send('Hello World!  Welcome to base camp')
 });
