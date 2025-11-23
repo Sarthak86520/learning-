4290a40e683622af518641ee4c0542d4a27d01b3
Sarthak Shukla <sarthakshukla9999@gmail.com>
2025-11-23 22:50:53 +0530

    basic backend for all project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./db/index.js";
+
 dotenv.config({
-    path: './.env',  
+  path: './.env',
 });
+
 const port = process.env.PORT || 3000;
 
-
 connectDB()
-.then()
-.catch((err) => {
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
     console.error("Failed to connect to the database:", err);
-    process.exit(1); 
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+    process.exit(1);
+  });
