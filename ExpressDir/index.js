2466ae082f74bd2b20f4719ba25bb23e01c42113
Sarthak Shukla <sarthakshukla9999@gmail.com>
2026-01-14 00:03:00 +0530

    Initial commit

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 let port = 3000;
-let host = 'localhost';
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
